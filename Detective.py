b806b29a79ce4dfa0915d091b9946770e7fe4e53
Schloz7 <40119096+Schloz7@users.noreply.github.com>
2021-09-20 14:07:33 +0100

    Add files via upload

---
+++ Detective.py
@@ -0,0 +1,183 @@
+import time
+from lifescroll import
+
+print(" Project \"CodeCademy\" Python Detective-Game")
+
+time.sleep(5)
+
+first_name = input(" Your name here KGB fellow:")
+
+time.sleep(5)
+
+print("You gonna live a day in a life of a KGB spy in the time of today")
+
+time.sleep(5)
+
+print("You're going to have 3 chances , can't be recovered like mistakes in real life")
+
+time.sleep(5)
+
+answer_1 = "RUSSIA"
+
+def favorite_first():
+ favorite_first = input("Say something to me right now , where was KGB created in first place ? ")
+ favorite_first = favorite_first.upper()
+ if favorite_first == answer_1:
+  print("That's the country where you're living right now")
+ else:
+  print("Try again") 
+ 
+ 
+
+favorite_first()
+
+time.sleep(5)
+print("You're in your office as a new member of the global order of secret services and living right now the most moment of supremacy ever in your home country")
+time.sleep(5)
+print("Your office is in moscow headquarters, filled with a beautiful black table and a new chair in white, the walls are painted in RED and with many ads of global enemies of the new order")
+time.sleep(5)
+print("You just gotta one of them and is a picture of a white male , famous for his large amount of crimes in the old soviet union, MURDERER , ROBBER and nevertheless enemy of your country")
+time.sleep(5)
+answer_2 = "ARGENTINE"
+
+def second_choice():
+ second_choice = input("Tell me , in which country has a flag with a sun in the middle and blue over and down : \n 1.Russia \n 2.Argentine \n 3.USA \n 4.France \n")
+ second_choice = second_choice.upper()
+ if second_choice == answer_2:
+  print("You're starting to understand the underworld more and know that this people are famous for their crimes all over the world of today , you just leave the room without talking and people \n are nothing talking to you")
+  time.sleep(5)
+  print("You leave to the street and see the beautiful city with russian girls everywhere and you kind look handsome , you leave to the airport and meet in your first class flight to \n Argentine a beautiful woman that sitting by your side all the flight , tells you that she has a place in front of the university \n UBA in argentine ")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+
+
+second_choice()
+time.sleep(10)
+print("Your flight arrived in Buenos Aires \n ezeiza airport \n you kinda happy for the invite to stay with the woman but you end refusing , you just used her to make you patch inside the country")
+time.sleep(8)
+print("You know two things about the suspect his age 50 and that he frequent a place called \"Puerto Madero\" , the place looks cool with girls everywhere \n you make your way in the night  ")
+time.sleep(8)
+print("The suspect in on a place with 2 \"capangas\" you saw him laughting about a stupid joke involving the police of the country ")
+time.sleep(5)
+
+answer_3 = "INVESTIGATE"
+
+def three_on_three():
+ three_on_three = input("BOOM! what happened ? , what??? there was a bomb and exploded , you're BLOODY, with the blood of people around you , DECIDE NOW : \n 1.Investigate \n 2.run like a chicken \n")
+ three_on_three = three_on_three.upper()
+ if three_on_three == answer_3:
+  print("Luck! , you can see a gun with the security guard and he is lying dead on the floor , you got his weapon a Magnum.357")
+  time.sleep(8)
+  print("The suspect obviously dissapeared , you gotta no time to look confuse and shot somebody that was stopping you to run to get your enemy that is nothing more that minutes in front of you,\n you just left the place")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+
+
+three_on_three()
+time.sleep(5)
+
+answer_4 = "LETHAL KICK"
+
+def three_on_four():
+ three_on_four = input("You're on Buenos Aires streets and running , you can see the desorientation of the police coming to the place where the bomb exploded , you're running to get \n your enemy,you even can see him alone a fat old pal running in front of the justice parlament,he gotta a Taxi pointing a gun to the driver and driving out\n there's a car with one person on it , but is a cop what do you do ? \n 1.Lethal Kick \n 2.think about it\n")
+ three_on_four = three_on_four.upper()
+ if three_on_four == answer_4:
+  time.sleep(5)  
+  print("You just got it , you're driving at a high speed following the suspect all over buenos aires ,speedy is the key , you are in better car and is almost getting him")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+
+
+three_on_four()
+time.sleep(5)
+
+answer_5 = "ANIMATE HIM"
+
+def animate_the():
+ animate_the = input("Crash!!!! you got him he is leaving with blood all over his head , you are not good neither and you got confused, you leave the car to get him \n you can hear the police coming towards you with their cars but they are really distant, you gotta in a fighting , after a while you \n could lead the situation and shot him on his chest , he is trying to run you know you must interrogate him , but he fell in the middle of the street. \n He is without breathing you have few minutes before police, what do you do? \n 1.Animate him \n 2.kill him \n 3.drive out\n")
+ animate_the = animate_the.upper()
+ if animate_the == answer_5:
+  print("That's correct . You need him a bit longer , he is almost diying you know that but you are good after all KGB , he is breathing again")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+
+
+animate_the()
+time.sleep(5)
+
+answer_6 = "NEVER"
+
+def animate_():
+ animate_ = input("He's crying and don't have to much time , you ask him informations about who he serves , he tells you finally and asks you to help him and don't let him to the police \n 1.Never \n 2.Of course \n")
+ animate_ = animate_.upper()
+ if animate_ == answer_6:
+  print("You left running to get a way out and you see the university in front of you , not a normal you see the medical school , UBA")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+
+
+animate_()
+
+time.sleep(5)
+answer_7 = "DISSECATION"
+
+def dissecated():
+ dissecated = input("You enter the university calling attention but you know how deal with and you enter a surgery room , no one's there just you , you wear a doctor's jacket and follow to the next door.\n you see students in the class studying you enter the class and the professor asks you \n Why you're late ? you tell him you're in the wrong class . but he insists and say you to stay , what do you do?\n 1.Dissecation \n 2.Go out \n ")
+ dissecated = dissecated.upper()
+ if dissecated == answer_7:
+  time.sleep(7)   
+  print("Excellent , the police is out running and you see the lights on the class windows . You watch the class")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+
+
+dissecated()
+time.sleep(5)
+answer_8 = "ELECTROCUTE HIM"
+
+def electro():
+ electro = input("You finish the class , sweating , you're walking through the hall and see that police is on the way out the university , you go up stairs you wanna use the windows to leave\n 1 , 2 , 3 floor no one there, you look at a room with many wires on the floor, you're going to the window and got surprised by a lieutenant of the police \n he asks you your ID,you are with a wire on your hand sparkling with high voltage \n 1.Give fake ID \n 2.Run \n 3.Electrocute him \n")
+ electro = electro.upper()
+ if electro == answer_8:
+  time.sleep(5)   
+  print("You did it , he died screaming , the way is clear now , you use the window and left going to the airport, but because you don't wanna call atention you go to that woman place that you\n meet coming to Argentine , she accept you and you have to gotta a ride untill the airport after talking and laughing she drove you till there")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+
+
+electro()
+time.sleep(5)
+answer_9 = "RUSSIA"
+
+def motherland():
+ motherland = input("She asks you about the ticket she have to buy and you say ?\n 1.Russia \n 2.Netherlands \n 3.France \n")
+ motherland = motherland.upper()
+ if motherland == answer_9:
+  print("Russia mother land!\n The flight departs after a goodbye and you're in the russian airport of Saint Petersburg , It's almost 23:00")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+time.sleep(5)
+
+motherland()
+time.sleep(5)
+answer_10 = "1"
+
+def first_place():
+ first_place = input("The best for the last , you got promotion and you're lieutenant , you commander wants you to answer this last question , what's the position of Russia in your life? \n 1.7 \n 2.4 \n 3.5 \n 4.6 \n 5.2 \n 6.3 \n 7.1\n")
+ first_place = first_place.upper()
+ if first_place == answer_10:
+  print("поздравление")
+ else:
+  #Untitled-3.Answers_script()
+  print("Try again") 
+
+
+first_place()
---
+++ lifescroll.py
@@ -0,0 +1,16 @@
+#Combat Function
+
+
+
+
+life = 3
+
+def Answers_script(life):  
+ if life >= 2:
+    new_life = life - 1  
+    print(new_life)
+ else:      
+    print("You lost and gotta captured by Scotland Yard , now you're going to live in the UK ,in a prison cell")
+ quit()
+
+
